3b9ed4a41e324f6600bf74f1ae1cad5af236ff6d
Shashvat Maurya <103407023+Shashvat300@users.noreply.github.com>
2023-04-20 01:21:44 +0530

    Create 3_Bit_Asynchronous_Down_Counter_Testbench.v
    The 3-bit Asynchronous Up Counter contains 3 T flip-flops and the T-input of all the flip-flops are connected to ‘1’. All these flip-flops are negative edge triggered but the outputs change asynchronously. The clock signal is directly applied to the first T flip-flop. So, the output of first T flip-flop toggles for every negative edge of clock signal.

---
+++ 3-Day [3-Bit Asynchronous Down Counter]/3_Bit_Asynchronous_Down_Counter_Testbench.v
@@ -0,0 +1,37 @@
+// _________________________________________________________________
+// Testbench........................................................
+// _________________________________________________________________
+
+
+module dc_tb();
+  
+  wire [2:0] COUNT;
+  reg CLK,RESET;
+  
+  Down_Counter inst1(.clk(CLK),.reset(RESET),.count(COUNT));
+  
+  initial
+    begin
+      CLK=0;
+      forever
+        #2 CLK=~CLK;
+    end
+  
+  initial
+    begin
+      $monitor("CLK=%b  RESET=%b COUNT=%d  TIME=%0t",CLK,RESET,COUNT,$time);
+      
+      RESET=0;
+      #10;
+      RESET=1;
+      #10;
+      RESET=0;
+    end
+  
+  initial
+    begin
+      $dumpfile("dump.vcd");
+      $dumpvars();
+      #200 $finish;
+    end
+endmodule
