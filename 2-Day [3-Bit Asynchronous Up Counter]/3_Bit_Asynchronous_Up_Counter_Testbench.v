68acbf548a2123261f6cda6171c67d12c69191cd
Shashvat Maurya <103407023+Shashvat300@users.noreply.github.com>
2023-04-19 21:30:15 +0530

    Create 3_Bit_Asynchronous_Up_Counter_Testbench.v

---
+++ 2-Day [3-Bit Asynchronous Up Counter]/3_Bit_Asynchronous_Up_Counter_Testbench.v
@@ -0,0 +1,38 @@
+// _________________________________________________________________
+// Testbench........................................................
+// _________________________________________________________________
+
+
+module tb_uc();
+  
+  wire [3:0] COUNT;
+  reg CLK,RESET;
+  
+  up_counter inst(.clk(CLK),.reset(RESET),.count(COUNT));
+  
+  initial
+    begin
+      CLK=0;
+      forever
+        #2 CLK=~CLK;
+    end
+  
+  initial
+    begin
+      $monitor("CLK=%b  RESET=%b OUTPUT COUNT=%d  TIME=%0t",CLK,RESET,COUNT,$time);
+     
+      RESET=0;
+      #10;
+      RESET=1;
+      #10
+      RESET=0;
+      
+    end
+  
+  initial
+    begin
+      $dumpfile("dump.vcd");
+      $dumpvars();
+      #100 $finish;
+    end
+endmodule
