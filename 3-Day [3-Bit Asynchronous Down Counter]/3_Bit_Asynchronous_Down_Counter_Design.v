5b125ce608c363211c487da5cc44acaa3f568768
Shashvat Maurya <103407023+Shashvat300@users.noreply.github.com>
2023-04-20 01:20:25 +0530

    Create 3_Bit_Asynchronous_Down_Counter_Design.v
    The 3-bit Asynchronous Up Counter contains 3 T flip-flops and the T-input of all the flip-flops are connected to ‘1’. All these flip-flops are negative edge triggered but the outputs change asynchronously. The clock signal is directly applied to the first T flip-flop. So, the output of first T flip-flop toggles for every negative edge of clock signal.

---
+++ 3-Day [3-Bit Asynchronous Down Counter]/3_Bit_Asynchronous_Down_Counter_Design.v
@@ -0,0 +1,18 @@
+// ___________________________________________________
+// 3-Bit Asynchronous Down Counter......................
+// ___________________________________________________
+ 
+
+module Down_Counter(count,clk,reset);
+  
+  output reg [2:0] count;
+  input clk, reset;
+  
+  always @(negedge clk)
+    begin
+      if(reset)
+        count=3'b111;
+      else
+        count=count-1;
+    end
+endmodule
