cb0167da2a51a91be6464a96744536fe16640b40
Shashvat Maurya <103407023+Shashvat300@users.noreply.github.com>
2023-05-13 23:37:05 +0530

    Create 3. Output.v
    D Flip Flop
    
    A D flip-flop, also known as a data flip-flop or delay flip-flop, is a fundamental building block of digital electronics. It is a type of sequential logic circuit that stores and transfers a single bit of data.
    
    The D flip-flop has two inputs: a data input (D) and a clock input (CLK). The output (Q) of the flip-flop reflects the value of the data input (D) at the moment when the clock signal transitions from low to high (rising edge). The flip-flop also has a complementary output (Q') which is the inverse of the output (Q).
    
    When the clock input is low, the flip-flop holds its current state, regardless of any changes on the data input. When the clock input goes high, the flip-flop takes the value of the data input and holds it until the next rising edge of the clock input. At that point, the flip-flop samples the input again and updates its output accordingly.
    
    D flip-flops are commonly used in digital circuits for applications such as data storage, synchronization, and control. They can be cascaded together to form more complex sequential circuits, such as counters, shift registers, and memory elements.

---
+++ 17. D Flip Flop/3. Output.v
@@ -0,0 +1,23 @@
+Chronologic VCS simulator copyright 1991-2021
+Contains Synopsys proprietary information.
+Compiler version S-2021.09; Runtime version S-2021.09;  May 13 14:03 2023
+CLK=1 D=0 Q=x Qbr=x  TIME=5
+CLK=1 D=1 Q=0 Qbr=1  TIME=15
+CLK=1 D=0 Q=1 Qbr=0  TIME=25
+CLK=1 D=1 Q=0 Qbr=1  TIME=35
+CLK=1 D=0 Q=1 Qbr=0  TIME=45
+CLK=1 D=1 Q=0 Qbr=1  TIME=55
+CLK=1 D=0 Q=1 Qbr=0  TIME=65
+CLK=1 D=1 Q=0 Qbr=1  TIME=75
+CLK=1 D=1 Q=1 Qbr=0  TIME=85
+CLK=1 D=1 Q=1 Qbr=0  TIME=95
+$finish called from file "testbench.sv", line 43.
+$finish at simulation time                  100
+           V C S   S i m u l a t i o n   R e p o r t 
+Time: 100 ns
+CPU Time:      0.500 seconds;       Data structure size:   0.0Mb
+Sat May 13 14:03:03 2023
+Finding VCD file...
+./dump.vcd
+[2023-05-13 14:03:04 EDT] Opening EPWave...
+Done
