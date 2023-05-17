8e7590f44f5eab6b25033cab56ec29374ed08a0b
Shashvat Maurya <103407023+Shashvat300@users.noreply.github.com>
2023-05-17 20:25:30 +0530

    Create 2. Output.v
    Non-Blocking Assignment
    
    
    Non-blocking assignment (<=) is a type of assignment statement used in Verilog for modeling sequential behavior in hardware designs. It is commonly used within procedural blocks, such as always blocks, to update values of variables concurrently without immediate effect.
    
    Here are some key points to understand about non-blocking assignments:
    
    Concurrent Execution: Non-blocking assignments allow multiple assignments to happen concurrently within a procedural block. All non-blocking assignments within the same block are scheduled to occur simultaneously, and the order of the assignments does not affect their execution.
    
    Deferred Update: Non-blocking assignments defer the update of the assigned variables until the end of the current time step or simulation cycle. This means that the assigned values do not take effect immediately in the current cycle but are stored and applied in the subsequent cycle.
    
    Synchronization with Clock Edge: Non-blocking assignments are typically used in synchronous designs and are sensitive to the clock edge (e.g., positive edge posedge clk or negative edge negedge clk). The assigned values are updated at the next clock edge, providing synchronized behavior.
    
    Avoiding Race Conditions: Non-blocking assignments help prevent race conditions that can occur when multiple assignments in the same block depend on each other's updated values. By deferring the update, non-blocking assignments ensure that the assigned values are consistent within the current cycle.
    
    Modeling Sequential Logic: Non-blocking assignments are commonly used to model flip-flops, registers, and other sequential elements in hardware designs. They allow the variables to be updated in a way that reflects the behavior of physical storage elements.

---
+++ 19. Swapping /2. Output.v
@@ -0,0 +1,13 @@
+Contains Synopsys proprietary information.
+Compiler version S-2021.09; Runtime version S-2021.09;  May 17 10:54 2023
+	 
+
+   Value Before Swapping a=10 & b=32                  0
+	 Value After Swapping a=10 & b=32                  10
+
+                         
+                         V C S   S i m u l a t i o n   R e p o r t 
+Time: 10 ns
+CPU Time:      0.480 seconds;       Data structure size:   0.0Mb
+Wed May 17 10:54:00 2023
+Done
