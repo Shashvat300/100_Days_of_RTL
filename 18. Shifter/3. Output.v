94dc3cd84220c5d5e082a87d9b77f9b557689f1a
Shashvat Maurya <103407023+Shashvat300@users.noreply.github.com>
2023-05-17 19:12:16 +0530

    Create 3. Output.v
    Shifter
    
    A shifter, also known as a shift register, is a digital circuit or operation that performs shifting of binary data or bits within a register. It allows the movement of bits in a specific direction (left or right) by a certain number of positions.
    
    In digital systems, shifters are commonly used for various purposes, including data manipulation, arithmetic operations, and implementation of certain algorithms. They are often implemented using flip-flops or other storage elements.
    
    There are two types of shifters based on the direction of shifting:
    
    1. Left Shifter: A left shifter moves the bits towards the left side of the register, pushing the leftmost bit out and inserting zeros at the rightmost side. Each bit is shifted to the adjacent higher position. It effectively multiplies the binary value by powers of 2.
    
    2. Right Shifter: A right shifter moves the bits towards the right side of the register, pushing the rightmost bit out and inserting zeros at the leftmost side. Each bit is shifted to the adjacent lower position. It effectively divides the binary value by powers of 2.

---
+++ 18. Shifter/3. Output.v
@@ -0,0 +1,17 @@
+Chronologic VCS simulator copyright 1991-2021
+Contains Synopsys proprietary information.
+Compiler version S-2021.09; Runtime version S-2021.09;  May 17 09:36 2023
+Time=0   DATA_IN=1011 SHIFT_AMOUNT=4  SHIFTED_DATA=10110000 
+Time=10   DATA_IN=1010 SHIFT_AMOUNT=3  SHIFTED_DATA=1 
+Time=20   DATA_IN=1101 SHIFT_AMOUNT=7  SHIFTED_DATA=11010000000 
+Time=30   DATA_IN=1011 SHIFT_AMOUNT=1  SHIFTED_DATA=101 
+$finish called from file "testbench.sv", line 40.
+$finish at simulation time                  100
+           V C S   S i m u l a t i o n   R e p o r t 
+Time: 100 ns
+CPU Time:      0.500 seconds;       Data structure size:   0.0Mb
+Wed May 17 09:36:55 2023
+Finding VCD file...
+./dump.vcd
+[2023-05-17 09:36:55 EDT] Opening EPWave...
+Done
