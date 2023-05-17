4c64cf33b9592087c4f32bbc19ed90c9ae573f82
Shashvat Maurya <103407023+Shashvat300@users.noreply.github.com>
2023-05-18 01:12:35 +0530

    Create 3. Output.v
    Traffic Signal Controller
    
    I've successfully developed a Verilog-based Traffic Signal Controller that simulates the behavior of a real-world traffic signal system. This project aims to enhance traffic management and create a realistic traffic flow simulation.
    
    The Traffic Signal Controller takes into account inputs such as clock signals, reset signals, and control signals to effectively manage the flow of traffic. It provides outputs for different traffic lights, including red, yellow, and green signals for each direction.
    
    During the development process, I carefully designed and implemented the Verilog code, ensuring efficient and accurate simulation of the traffic signal system. I incorporated key algorithms and optimizations to handle traffic flow and timings effectively.
    
    The results of the project are promising, with the Traffic Signal Controller accurately representing real-world traffic scenarios. Through extensive testing and simulations, I have validated the effectiveness of the controller in managing traffic flow and optimizing signal timings.
    
    Throughout this project, I encountered various challenges, including algorithm design, synchronization, and optimization. Overcoming these challenges allowed me to enhance my skills in Verilog coding and simulation techniques.

---
+++ 20. Traffic Signal Controller/3. Output.v
@@ -0,0 +1,20 @@
+Chronologic VCS simulator copyright 1991-2021
+Contains Synopsys proprietary information.
+Compiler version S-2021.09; Runtime version S-2021.09;  May 17 15:33 2023
+	 CLR=x X=x HWY=x CNTRY=x   TIME=0
+	 CLR=1 X=x HWY=x CNTRY=x   TIME=3
+	 CLR=1 X=1 HWY=2 CNTRY=0   TIME=5
+	 CLR=0 X=1 HWY=2 CNTRY=0   TIME=7
+	 CLR=0 X=1 HWY=1 CNTRY=0   TIME=15
+	 CLR=0 X=1 HWY=0 CNTRY=0   TIME=55
+	 CLR=0 X=1 HWY=0 CNTRY=2   TIME=85
+	 CLR=0 X=0 HWY=0 CNTRY=1   TIME=105
+	 CLR=0 X=0 HWY=2 CNTRY=0   TIME=145
+	 CLR=0 X=1 HWY=1 CNTRY=0   TIME=205
+	 CLR=0 X=1 HWY=0 CNTRY=0   TIME=245
+	 CLR=0 X=1 HWY=0 CNTRY=2   TIME=275
+$stop at time 305 Scope: tb_tsc File: testbench.sv Line: 48
+ucli% Finding VCD file...
+./dump.vcd
+[2023-05-17 15:33:50 EDT] Opening EPWave...
+Done
