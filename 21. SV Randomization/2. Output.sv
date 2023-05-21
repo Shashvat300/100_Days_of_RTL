1a44fcabe29682e08b210e089f20dbab85371e18
Shashvat Maurya <103407023+Shashvat300@users.noreply.github.com>
2023-05-21 20:42:55 +0530

    Create 2. Output.sv
    SystemVerilog Randomization
    
    
    SystemVerilog provides powerful features for randomization, which allow for efficient and effective generation of randomized test stimuli during verification. Randomization is a technique used to generate a wide variety of input stimuli to thoroughly test a design or module, uncovering potential corner cases and verifying its functionality under different scenarios.
    
    In SystemVerilog, randomization is commonly used with the rand and randc keywords, along with other related constructs. Here are the key elements of SystemVerilog randomization:
    
    1. Random variables are declared using the rand keyword. They can be of any supported SystemVerilog data type, such as integer, bit, enum, struct, or class. Random variables are used to represent the input or stimulus to be randomized during test generation.
    
    
    2. n SystemVerilog, the randc keyword is used to declare a random variable that takes on values in a cyclic sequence The randc keyword stands for "random cyclic" and is similar to the rand keyword used for declaring random variables. However, while a rand variable generates values randomly from a specified range or set of values, a randc variable cycles through a predefined sequence of values.

---
+++ 21. SV Randomization/2. Output.sv
@@ -0,0 +1,28 @@
+Chronologic VCS simulator copyright 1991-2021
+Contains Synopsys proprietary information.
+Compiler version S-2021.09; Runtime version S-2021.09;  May 21 11:09 2023
+	 Rndom Value of rand_a= 3 & randc_b= 5
+	 Rndom Value of rand_a= 6 & randc_b= 1
+	 Rndom Value of rand_a= 1 & randc_b= 2
+	 Rndom Value of rand_a= 9 & randc_b= 3
+	 Rndom Value of rand_a=13 & randc_b= 4
+	 Rndom Value of rand_a= 7 & randc_b= 0
+	 Rndom Value of rand_a= 0 & randc_b= 7
+	 Rndom Value of rand_a= 2 & randc_b= 9
+	 Rndom Value of rand_a=13 & randc_b= 6
+	 Rndom Value of rand_a=10 & randc_b=14
+	 Rndom Value of rand_a=15 & randc_b=12
+	 Rndom Value of rand_a=15 & randc_b= 8
+	 Rndom Value of rand_a=15 & randc_b=11
+	 Rndom Value of rand_a=14 & randc_b=10
+	 Rndom Value of rand_a=10 & randc_b=13
+	 Rndom Value of rand_a=11 & randc_b=15
+	 Rndom Value of rand_a= 4 & randc_b= 1
+	 Rndom Value of rand_a= 3 & randc_b=14
+	 Rndom Value of rand_a= 8 & randc_b=15
+	 Rndom Value of rand_a= 6 & randc_b= 0
+           V C S   S i m u l a t i o n   R e p o r t 
+Time: 0 ns
+CPU Time:      0.500 seconds;       Data structure size:   0.0Mb
+Sun May 21 11:09:09 2023
+Done
