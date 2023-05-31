70148ad71145025e817f131f1725927e1e3f70d6
Shashvat Maurya <103407023+Shashvat300@users.noreply.github.com>
2023-05-31 23:20:45 +0530

    Create 5. Output.sv
    **SV Mailbox
    **
    
    In SystemVerilog, a mailbox is a built-in synchronization primitive used for communication and data exchange between multiple concurrent processes or threads. It provides a simple and efficient way to transfer data among different components of a design.
    
    A mailbox consists of a fixed-size buffer that can hold one or more messages. Each message can be of any user-defined data type. The mailbox supports two fundamental operations: put and get.
    
    * Put Operation: The put operation is used to insert a message into the mailbox. If the mailbox is full, the put operation will block until space becomes available.
    
    
    * Get Operation: The get operation is used to retrieve a message from the mailbox. If the mailbox is empty, the get operation will block until a message becomes available.
    
    The put and get operations can be performed by different processes or threads, allowing for asynchronous communication between them. The mailbox ensures proper synchronization and mutual exclusion, preventing data races and ensuring safe data exchange.
    
    SystemVerilog provides the mailbox keyword to declare a mailbox variable

---
+++ 23. SV Mailbox/5. Output.sv
@@ -0,0 +1,21 @@
+Contains Synopsys proprietary information.
+Compiler version S-2021.09; Runtime version S-2021.09;  May 31 13:45 2023
+-----------------------------------------
+	 Packet :: Packet Generated
+Packet :: a=0 b=1
+Generator :: Packet put into Mailbox
+Driver :: Packet Recived
+Drive :: a=0 b=1 
+ 
+	 Packet :: Packet Generated
+Packet :: a=1 b=0
+Generator :: Packet put into Mailbox
+Driver :: Packet Recived
+Drive :: a=1 b=0 
+ 
+----------------------------------------
+           V C S   S i m u l a t i o n   R e p o r t 
+Time: 10 ns
+CPU Time:      0.400 seconds;       Data structure size:   0.0Mb
+Wed May 31 13:45:50 2023
+Done
